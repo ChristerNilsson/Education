a0b9672413955ae0cd64225b9ec15f2fc80aeed0
christer@DEC2011 <janchrister.nilsson@gmail.com>
2014-11-06 04:02:33 +0100

    Some changes

--- CalcRPN/Ruby/start.rb
+++ CalcRPN/Ruby/start.rb
@@ -1,29 +1,8 @@
-# https://github.com/ChristerNilsson/Education/blob/master/CalcRPN/Ruby/start.rb
-
 require 'prime'
-
-
-def show_ass a,b
-  puts "#{a} != #{b}"
-  caller.each { |x| puts x if x.include?('assertions') && x.include?('top') }
-end
-
-def ass a,b,decs=6
-  case a.class.name
-  when 'Array'
-    return show_ass a,b if a.size != b.size
-    a = a.map { |x| x.class.name=='Float' ? x.round(decs) : x }
-    b = b.map { |x| x.class.name=='Float' ? x.round(decs) : x }
-    show_ass a,b if a != b
-  when 'Float'
-    show_ass a,b if a.round(decs) != b.round(decs)
-  else
-    show_ass a,b if a != b
-  end
-end
 
 class CalcRPN
   def initialize
+    @defs = {}
     reset
 
     @words0 = {}
@@ -36,6 +15,7 @@ class CalcRPN
 
     @words2 = {}
     @words2['+'] = -> x,y {@stack << y + x}
+
   end
 
   def reset
@@ -43,16 +23,6 @@ class CalcRPN
   end
 
   def prompt
-    #stack = @stack.map do |x|
-    #  case x.class.name
-    #  when 'Float'
-    #    x.round(@round)
-    #  when 'Fixnum'
-    #    x.to_s(@base)
-    #  else
-    #    x
-    #  end
-    #end
     stack = @stack.to_s.gsub('"','')
     "#{stack} Command: "
   end
@@ -71,31 +41,21 @@ class CalcRPN
 
     lst.each do |x|
 
-      word = @words0[x]
-      if word
-        return if word.call == nil
-        next
-      end
-
-      word = @words1[x]
-      if word
-        if @stack.size < 1
-          puts "stack too small for #{x}"
-          return
+      words = [@words0[x],@words1[x],@words2[x]]
+      words.each_with_index do |word,count|
+        if word
+          if @stack.size < count
+            puts "stack too small for #{x}"
+            return
+          end
+          case count
+          when 0; return if word.call() == nil
+          when 1; return if word.call(@stack.pop) == nil
+          when 2; return if word.call(@stack.pop,@stack.pop) == nil
+          end
         end
-        return if word.call(@stack.pop) == nil
-        next
       end
-
-      word = @words2[x]
-      if word
-        if @stack.size < 2
-          puts "stack too small for #{x}"
-          return
-        end
-        return if word.call(@stack.pop,@stack.pop) == nil
-        next
-      end
+      next if words != [nil,nil,nil]
 
       x = read_number(x)
       @stack << x if x
@@ -120,12 +80,6 @@ class CalcRPN
     x.include?('.')
   end
 
-  def run input, output=[],round=6
-    reset
-    calc input.split
-    ass @stack,output,round
-  end
-
   def loop
     run '?'
     reset
@@ -135,13 +89,25 @@ class CalcRPN
     end
   end
 
+  def define s
+    name, *@defs[name] = s.split
+  end
+
+  def run input
+    return if input==''
+    reset
+    calc input.split
+    @stack
+  end
+
 end
 
-def define_externals
-  #@c.run(': sq dup *') # n -- n
+def define_externals rpn
+  rpn.define 'dbl 2 *'
 end
 
-@c = CalcRPN.new
-define_externals
 require './assertions'
-@c.loop
+
+rpn = CalcRPN.new
+define_externals rpn
+rpn.loop
